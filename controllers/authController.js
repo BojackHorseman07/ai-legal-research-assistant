1925b098afc4bda16d2860ff7d719cafbff4ec36
BojackHorseman07 <abhijithhorseman07@gmail.com>
2026-06-12 15:58:52 +0530

    day3 login authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const register = async (req, res) => {
@@ -29,5 +30,36 @@ const register = async (req, res) => {
 
     }
 };
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+    return res.status(404).json({
+        message: "User not found"
+    });
+}
+const isMatch = await bcrypt.compare(
+    password,
+    user.password
+);
+if (!isMatch) {
+    return res.status(401).json({
+        message: "Invalid credentials"
+    });
+}
+const token = jwt.sign(
+    {
+        userId: user._id
+    },
+    process.env.JWT_SECRET,
+    {
+        expiresIn: "1h"
+    }
+);
+return res.status(200).json({
+    message: "Login successful",
+    token
+});
+};
 
-module.exports = { register };
+module.exports = { register,login };
